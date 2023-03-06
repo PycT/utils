cbfe1e829e1b75320b4e9f4b010761594e3e54dd
pyct <rustem.zakiev@gmail.com>
2023-03-06 15:44:29 +0100

    char_grandes.py

--- src/chargrandes.py
+++ src/chargrandes.py
@@ -1,22 +1,3 @@
-#!python
-
-# An utility to pick a random number from a range with no repeating
-# The range is defined with the variables range_start and range_end
-# (lines 14 and 15)
-#
-# The numbers used are stored in "picks.json"
-# usage: ./pick.py
-
-
-import random
-import json
-
-range_start = 1
-range_end = 62
-
-pick = 0
-
-
 chars = {
     "0": [
         "  000000000000  ",
@@ -132,26 +113,3 @@ def print_grandes(the_string):
     print("|  {}  |".format(" " * len(the_string) * 16))
     print(" --{}-- ".format("-" * len(the_string) * 16))
  
-
-if __name__ == "__main__":
-    try:
-        with open("picks.json", "r") as past_records:
-            past = json.load(past_records)
-    except Exception as e:
-        print(e)
-        past = {"past": [0]}
-        print("'picks.json' file created.")
-    
-    if len(past) > range_end:
-        print("Range exceeded")
-        print("clear the 'picks.json' file")
-        exit()
-
-    while pick in past["past"]:
-        pick = random.randrange(range_start, range_end)
-
-    print_grandes(str(pick))
-    past["past"].append(pick)
-
-    with open("picks.json", "w") as past_records:
-        json.dump(past, past_records, indent = 4)
